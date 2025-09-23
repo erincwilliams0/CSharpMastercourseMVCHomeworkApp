7c845b0c99f9ec68f31f56461a5a5207a4752b7e
erin williams <erin.cwilliams0@gmail.com>
2025-09-23 08:32:31 -0500

    Add project files.

---
+++ MVCHomework/Models/GreetingModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCHomework.Models
+{
+    public class GreetingModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+    }
+}
